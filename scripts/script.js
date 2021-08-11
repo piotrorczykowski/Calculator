8f7a06dcc3a1da862e0aa3922e1455863a9c271d
Piotr Orczykowski <orczykowski.peter@gmail.com>
2021-08-11 12:04:32 +0200

    Added function for theme changing

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,19 @@ function clearScreen()
 {
     document.getElementById('result').value = ""
 }
+
+function changeTheme()
+{
+    var currentTheme = document.getElementById('theme');
+
+    if(currentTheme.getAttribute('href') === '/styles/light.css')
+    {
+        currentTheme.href = '/styles/dark.css'
+        document.getElementById('changeTheme').value = 'Light Mode';
+    }
+    else
+    {
+        currentTheme.href = '/styles/light.css'
+       document.getElementById('changeTheme').value = 'Dark Mode';
+    }
+}
